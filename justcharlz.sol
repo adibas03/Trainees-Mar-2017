aedf070b90e6135b8866bb71912321fc1c86cbd7
justcharlz <charlie4biz@gmail.com>
2017-04-03 15:13:20 +0100

    Update justcharlz.sol

--- justcharlz.sol
+++ justcharlz.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.4.9;
 contract aboutme{
     
     address public owner;
-    uint count;
     
     struct justme{
             bytes32 dapp_choice;
@@ -13,12 +12,12 @@ contract aboutme{
             bytes32 asset_quality;
             bytes32 whatissuperdao;
     }
-    justme[] mybio;
+    justme mybio;
     
     
     function aboutme(){
         owner = msg.sender;
-        count = 0;
+        
     }
     
     function set(bytes32 choice,uint exp, bytes32 myrole, bytes32 languages, bytes32 asset, bytes32 superdao )
@@ -27,24 +26,24 @@ contract aboutme{
          if(msg.sender != owner ) 
          {return;}
          
-        mybio[count].dapp_choice = choice;
-        mybio[count].experience = exp;
-        mybio[count].role = myrole;
-        mybio[count].program_language_experience = languages;
-        mybio[count].asset_quality = asset;
-        mybio[count].whatissuperdao = superdao;
+        mybio.dapp_choice = choice;
+        mybio.experience = exp;
+        mybio.role = myrole;
+        mybio.program_language_experience = languages;
+        mybio.asset_quality = asset;
+        mybio.whatissuperdao = superdao;
          
         return true;
     }
     
-    function get(uint sn) constant returns(bytes32,uint,bytes32,bytes32,bytes32,bytes32){
+    function get() constant returns(bytes32,uint,bytes32,bytes32,bytes32,bytes32){
         return(
-            mybio[sn].dapp_choice,
-            mybio[sn].experience,
-            mybio[sn].role,
-            mybio[sn].program_language_experience,
-            mybio[sn].asset_quality,
-            mybio[sn].whatissuperdao
+            mybio.dapp_choice,
+            mybio.experience,
+            mybio.role,
+            mybio.program_language_experience,
+            mybio.asset_quality,
+            mybio.whatissuperdao
             );
         
     }

2b12dd5cbdb41f4e3c30f37bdf29005e3fc11daa
L'herbeur <lherbeur@gmail.com>
2017-04-04 09:17:08 +0100

    Update lherbeur.sol

--- lherbeur.sol
+++ lherbeur.sol
@@ -1,27 +1,25 @@
 pragma solidity ^0.4.10;
-contract LherbeurContract {
+contract Lherbeur {
     
     address public owner;
+    
+    struct Info
+    {
     bytes32 dapChoice;
     bytes32 expectedRole;
     uint yearsOfExperience;
     bytes32 progLang;
     bytes32 assetQuality;
     bytes32 whatIsSuperDAO;
+    }
+    
+    Info info;
     
     function LherbeurContract()
     {
         owner = msg.sender;
     }
-    
-    // function checkOwner(address executor) returns (bool)
-    // {
-    //     if (executor == owner)
-    //     return true;
-    //     else 
-    //     return false;
-    // }
-    
+   
     modifier accessGrant(address _executor)
     {
         if (_executor != owner)
@@ -30,92 +28,23 @@ contract LherbeurContract {
         _;
     }
     
-    function setdapChoice(bytes32 dap) 
-        accessGrant (owner)
+    function setInfo(bytes32 dap, bytes32 role, uint yearsExp, bytes32 lang, bytes32 quality, bytes32 superDAOWhat) 
+      accessGrant (owner)
     { 
-        dapChoice = dap; 
+        info.dapChoice = dap; 
+        info.expectedRole = role; 
+        info.yearsOfExperience = yearsExp; 
+        info.progLang = lang; 
+        info.assetQuality = quality; 
+        info.whatIsSuperDAO = superDAOWhat; 
+    }
+   
+   function getInfo() public constant 
+   accessGrant (owner)
+   returns (bytes32, bytes32, uint, bytes32, bytes32, bytes32) 
+    { 
+        return (info.dapChoice, info.expectedRole, info.yearsOfExperience, info.progLang, info.assetQuality, info.whatIsSuperDAO); 
     }
     
-    function getdapChoice() 
-        accessGrant (owner)
-        returns(bytes32) 
-    { 
-        return dapChoice; 
-        
-    }
-    
-    function setexpectedRole(bytes32 role) 
-        accessGrant (owner)
-    { 
-        expectedRole = role; 
-    }
-    
-    function getexpectedRole() 
-        accessGrant (owner)
-        returns(bytes32) 
-    { 
-        return expectedRole; 
-        
-    }
-
- 
-    function setyearsOfExperience(uint yearsExp) 
-        accessGrant (owner)
-    { 
-        yearsOfExperience = yearsExp; 
-    }
-    
-    function getyearsOfExperience() 
-        accessGrant (owner)
-        returns(uint) 
-    { 
-        return yearsOfExperience; 
-        
-    }
-
-    
-    function setprogLang(bytes32 lang) 
-        accessGrant (owner)
-    { 
-        progLang = lang; 
-    }
-    
-    function getprogLang() 
-        accessGrant (owner)
-        returns(bytes32) 
-    { 
-        return progLang; 
-        
-    }
-    
-     
-    function setassetQuality(bytes32 quality) 
-        accessGrant (owner)
-    { 
-        assetQuality = quality; 
-    }
-    
-    function getassetQuality() 
-        accessGrant (owner)
-        returns(bytes32) 
-    { 
-        return assetQuality; 
-        
-    }
-    
-     
-    function setwhatIsSuperDAO(bytes32 superDAOWhat) 
-        accessGrant (owner)
-    { 
-        whatIsSuperDAO = superDAOWhat; 
-    }
-    
-    function getwhatIsSuperDAO() 
-        accessGrant (owner)
-        returns(bytes32) 
-    { 
-        return whatIsSuperDAO; 
-        
-    }
     
 }
